b145e8f66f79ac18a6ff1620428723a9abd0d1b8
Sean Alden <sean.alden@great.sch.id>
2026-04-25 09:54:04 +0700

    update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,13 +2,12 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends Factory<User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
@@ -25,11 +24,20 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            // UBAH 'name' menjadi 'first_name' dan 'last_name'
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+
+            // Tambahkan field lain yang wajib (NOT NULL) jika tidak punya default value di DB
+            'usertype' => 'user',
+            'is_membership' => 0,
+            'point' => 0,
+            'is_subscribed' => 0,
         ];
     }
 
