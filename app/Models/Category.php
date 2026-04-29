b6a048383cb3258d5107965f4a01a3558c37d0be
Sean Alden <sean.alden@great.sch.id>
2026-04-29 17:37:40 +0700

    add audit log #3

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\Auditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
+    use Auditable;
     use HasFactory;
     protected $fillable = ['code', 'name', 'description'];
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -71,6 +71,7 @@
 
 namespace App\Models;
 
+use App\Traits\Auditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -78,6 +79,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Product extends Model
 {
     use HasFactory;
+    use Auditable;
 
     protected $fillable = [
         'category_id',
